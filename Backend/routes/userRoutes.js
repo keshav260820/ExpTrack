fef89509d8e915a3844e2d802f36759980256593
Mew <shallychauhan127@gmail.com>
2026-03-09 13:58:02 +0530

    deleted whole projectttt

--- Backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router  = express.Router();
-const { registerUser, loginUser, getProfile } = require('../controller/userController');
-
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-router.get('/:id', getProfile);
-
-module.exports = router;
